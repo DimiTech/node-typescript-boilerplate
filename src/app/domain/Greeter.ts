5e664888bebc92dc879cba68e0d2f7be0a82c138
Dušan Dimitrić <dusan_dimitric@yahoo.com>
2019-04-18 13:12:53 +0200

    Clean up Greeter

--- src/app/domain/Greeter.ts
+++ src/app/domain/Greeter.ts
@@ -2,8 +2,7 @@ import IGreeter from '@app/domain/IGreeter'
 
 class Greeter implements IGreeter {
   public greet(name: string) {
-    process.stdout.write(`Hello ${name}!`)
-    // throw new Error('kako ide')
+    process.stdout.write(`Hello ${name}!\n`)
   }
 }
 
