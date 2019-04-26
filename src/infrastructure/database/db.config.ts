540111eead9a10835518cafae95581fc8c612e7c
Dusan Dimitric <dusan@mitipi.com>
2019-04-26 12:21:31 +0200

    Add MongoDB to TravisCI

--- src/infrastructure/database/db.config.ts
+++ src/infrastructure/database/db.config.ts
@@ -7,9 +7,9 @@ export interface IDBConfig {
 }
 
 export const DBConfig: IDBConfig = {
-  host: process.env.DB_HOST || '',
-  user: process.env.DB_USER || '',
-  pass: process.env.DB_PASS || '',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  dbName: process.env.DB_NAME || '',
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 'test',
+  pass: process.env.DB_PASS || 'test',
+  port: parseInt(process.env.DB_PORT || '27017', 10),
+  dbName: process.env.DB_NAME || 'test_db',
 }
