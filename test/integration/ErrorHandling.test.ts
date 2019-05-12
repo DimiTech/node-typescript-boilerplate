1302b59d65d35e1d6eb79f8fddcc826c7c8f2ff9
Dusan Dimitric <dusan@mitipi.com>
2019-05-12 15:39:43 +0200

    Rename error handling test suite

--- test/integration/ErrorHandling.test.ts
+++ test/integration/ErrorHandling.test.ts
@@ -12,4 +12,14 @@ describe('Web API global error handling', () => {
         done()
       })
   })
+
+  // TODO: Find a way to test this out? Trigger an error somehow !?
+  // it('Returns a 500 Server Error when an error bubbles up to the catch-all express error handler', done => {
+  //   request(server)
+  //     .post('/nonexistent')
+  //     .end((_err, res) => {
+  //       expect(res.status).to.equal(404)
+  //       done()
+  //     })
+  // })
 })
